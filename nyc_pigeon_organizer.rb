c3a775861c716bf19790df56482d3d0d1c2160f0
Bleak Chandler <bleakchandler@gmail.com>
2021-01-26 22:46:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,4 @@
-nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer(data)
 final_results = data.each_with_object({}) do |(key, value), final_array|
   value.each do |inner_key, names|
     names.each do |name|
